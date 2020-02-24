01e5809889d80013d5c7ec734062494570ebadcf
Hong <shong202@cable.comcast.com>
2020-02-24 17:07:51 -0500

    Added new programming assignment.

---
+++ coursera_u_of_i/assignment2/graph.js
@@ -0,0 +1,30 @@
+var margin = {left:50,right:50,top:40,bottom:50};
+
+var height = 600;
+var width = 860;
+
+var svg = d3.select("svg")
+
+var color = d3.scaleOrdinal(d3.schemeCategory20);
+
+var simulation = d3.forceSimulation()
+    .force("link", d3.forceLink().id(function(d) { return d.id; }))
+    .force("charge", d3.forceManyBody())
+    .force("center", d3.forceCenter(width / 2, height / 2));
+
+function dragstarted(d) {
+  if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+  d.fx = d.x;
+  d.fy = d.y;
+}
+
+function dragged(d) {
+  d.fx = d3.event.x;
+  d.fx = d3.event.y;
+}
+
+function dragended(d) {
+  if (!d3.event.active) simulation.alphaTarget(0);
+  d.fx = null;
+  d.fy = null;
+}
