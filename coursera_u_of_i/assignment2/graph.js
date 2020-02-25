b4f79187f3fa534d06489790e8e06eced52276f8
Hong <shong202@cable.comcast.com>
2020-02-25 15:10:40 -0500

    added new json.

--- coursera_u_of_i/assignment2/graph.js
+++ coursera_u_of_i/assignment2/graph.js
@@ -1,42 +1,77 @@
 var margin = {left:50,right:50,top:40,bottom:50};
 
-var height = 600;
-var width = 860;
+//var height = 600;
+//var width = 860;
 
-var svg = d3.select("svg");
+var svg = d3.select("svg"),
+    width = +svg.attr("width"),
+    height = +svg.attr("height");
 
-/*
-d3.csv("https://raw.githubusercontent.com/dogherder/datavisualization/master/coursera_u_of_i/assignment2/9_11_HIJACKERS_ADJACENCY_LIST.csv")
-  .row(function(d) {
-    return { source: d.source, target: d.target} })
-  .get(function(error,data) {
-    if(error) throw error;
-
-    var link = svg.append("g")
-        .attr("class", "links")
-      .selectAll("line")
-      .data(data.links)
-      .enter().append("line")
-        .attr("stroke-width", function(d) {return Math.sqrt(d.value); });
-}); */
-
-console.log(links_data[0].links[0].source);
-
-var node_data = d3.csv("https://raw.githubusercontent.com/dogherder/datavisualization/master/coursera_u_of_i/assignment2/9_11_HIJACKERS_ATTR.csv").get(function(error,data) {
-  return {
-    data
-  }
-});
-
-//console.log(node_data);
-
-/*
 var color = d3.scaleOrdinal(d3.schemeCategory20);
 
 var simulation = d3.forceSimulation()
     .force("link", d3.forceLink().id(function(d) { return d.id; }))
     .force("charge", d3.forceManyBody())
     .force("center", d3.forceCenter(width / 2, height / 2));
+
+d3.json("./hijackers_adjacency_list.json", function(error, graph) {
+  if (error) throw error;
+
+  var link = svg.append("g")
+        .attr("class", "links")
+      .selectAll("line")
+      .data(graph.links)
+      .enter().append("line")
+          .attr("stroke-width", function(d) {return "1"; });
+
+  var node = svg.append("g")
+        .attr("class", "nodes")
+      .selectAll("g")
+      .data(graph.nodes)
+      .enter().append("g")
+
+  var circles = node.append("circle")
+      .attr("r", 5)
+      .attr("fill", function(d) { return color(d.ties); })
+      .call(d3.drag()
+          .on("start", dragstarted)
+          .on("drag", dragged)
+          .on("end", dragended));
+
+  var labels = node.append("text")
+      .text(function(d) {
+        d.terrorist;
+      })
+      .attr('x', 6)
+      .attr('y', 3);
+
+  node.append("title")
+      .text(function(d) { return d.terrorist});
+
+  simulation
+      .nodes(network_data[0].nodes)
+      .on("tick", ticked);
+
+  simulation.force("link")
+      .links(network_data[0].links);
+
+  function ticked() {
+    link
+        .attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.x; })
+        .attr("x2", function(d) { return d.source.x; })
+        .attr("y2", function(d) { return d.source.x; });
+
+    node
+        .attr("transformation", function(d) {
+          return "translate(" + d.x + "," + d.y + ")";
+        })
+  }
+
+});
+
+
+//console.log(network_data[0]);
 
 function dragstarted(d) {
   if (!d3.event.active) simulation.alphaTarget(0.3).restart();
@@ -54,5 +89,3 @@ function dragended(d) {
   d.fx = null;
   d.fy = null;
 }
-
-*/
