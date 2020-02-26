089ffdb2df4dc3bed4697d72fafdfc42e4606a1a
Hong <shong202@cable.comcast.com>
2020-02-25 21:18:50 -0500

    Modified assignment 2.

--- coursera_u_of_i/assignment2/graph.js
+++ coursera_u_of_i/assignment2/graph.js
@@ -11,18 +11,21 @@ var color = d3.scaleOrdinal(d3.schemeCategory20);
 
 var simulation = d3.forceSimulation()
     .force("link", d3.forceLink().id(function(d) { return d.id; }))
-    .force("charge", d3.forceManyBody())
+    .force("charge", d3.forceManyBody().strength(-10))
+    .force("collide", d3.forceCollide(40).strength(.5).iterations(1))
+    .force("x", d3.forceX(width/2).strength(.015))
+    .force("y", d3.forceY(height/2).strength(.015))
     .force("center", d3.forceCenter(width / 2, height / 2));
 
-d3.json("./hijackers_adjacency_list.json", function(error, graph) {
+d3.json("https://raw.githubusercontent.com/dogherder/datavisualization/master/coursera_u_of_i/assignment2/hijackers_adjacency_list.json", function(error, graph) {
   if (error) throw error;
-
+  console.log(graph)
   var link = svg.append("g")
         .attr("class", "links")
       .selectAll("line")
       .data(graph.links)
       .enter().append("line")
-          .attr("stroke-width", function(d) {return "1"; });
+          .attr("stroke-width", function(d) {return d.strength^2; });
 
   var node = svg.append("g")
         .attr("class", "nodes")
@@ -40,7 +43,7 @@ d3.json("./hijackers_adjacency_list.json", function(error, graph) {
 
   var labels = node.append("text")
       .text(function(d) {
-        d.terrorist;
+        return d.terrorist;
       })
       .attr('x', 6)
       .attr('y', 3);
@@ -49,21 +52,21 @@ d3.json("./hijackers_adjacency_list.json", function(error, graph) {
       .text(function(d) { return d.terrorist});
 
   simulation
-      .nodes(network_data[0].nodes)
+      .nodes(graph.nodes)
       .on("tick", ticked);
 
   simulation.force("link")
-      .links(network_data[0].links);
+      .links(graph.links);
 
   function ticked() {
     link
         .attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.x; })
-        .attr("x2", function(d) { return d.source.x; })
-        .attr("y2", function(d) { return d.source.x; });
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
 
     node
-        .attr("transformation", function(d) {
+        .attr("transform", function(d) {
           return "translate(" + d.x + "," + d.y + ")";
         })
   }
@@ -81,7 +84,7 @@ function dragstarted(d) {
 
 function dragged(d) {
   d.fx = d3.event.x;
-  d.fx = d3.event.y;
+  d.fy = d3.event.y;
 }
 
 function dragended(d) {
@@ -89,3 +92,78 @@ function dragended(d) {
   d.fx = null;
   d.fy = null;
 }
+
+var legendpositionleft = width - 330;
+var legendpositiontop = height - 220;
+
+var legendGroup = svg.append("g").attr("class", "legend-group").attr("transform", "translate("+legendpositionleft+","+legendpositiontop+")");
+
+var legend = legendGroup.append("rect")
+        .attr("height", 85)
+        .attr("width", 135)
+        .attr("transform", "translate("+125+","+60+")")
+        .style("stroke", "gray")
+        .style("fill", "none");
+var text1 = legendGroup.append("text")
+        .attr("y", 55)
+        .attr("x", 125)
+        .style("text-anchor", "start")
+        .style("font-family", "Monospace")
+        .text("LEGEND");
+
+var circle1 = legendGroup.append("circle")
+        .attr("r", 5)
+        .attr("cx", 140)
+        .attr("cy", 72)
+        .style("fill", "#ffbb78");          //#ffbb78
+var circle2 = legendGroup.append("circle")
+        .attr("r", 5)
+        .attr("cx", 140)
+        .attr("cy", 88)
+        .style("fill", "#1f77b4")         //"#1f77b4
+var circle3 = legendGroup.append("circle")
+        .attr("r", 5)
+        .attr("cx", 140)
+        .attr("cy", 102)
+        .style("fill", "#aec7e8");        //"#aec7e8
+var circle4 = legendGroup.append("circle")
+        .attr("r", 5)
+        .attr("cx", 140)
+        .attr("cy", 117)
+        .style("fill", "#ff7f0e");        //"#ff7f0e
+var circle5 = legendGroup.append("circle")
+        .attr("r", 5)
+        .attr("cx", 140)
+        .attr("cy", 132)
+        .style("fill", "#2ca02c");        //#2ca02c
+
+var text1 = legendGroup.append("text")
+        .attr("y", 75)
+        .attr("x", 160)
+        .style("text-anchor", "start")
+        .style("font-family", "Monospace")
+        .text("AA #11 WTC North");        //AA #11 WTC North
+var text2 = legendGroup.append("text")
+        .attr("y", 90)
+        .attr("x", 160)
+        .style("text-anchor", "start")
+        .style("font-family", "Monospace")
+        .text("AA #77 Pentagon");        //AA #77 Pentagon
+var text3 = legendGroup.append("text")
+        .attr("y", 105)
+        .attr("x", 160)
+        .style("text-anchor", "start")
+        .style("font-family", "Monospace")
+        .text("UA #93 Pennsylvania");        //UA #93 Pennsylvania
+var text4 = legendGroup.append("text")
+        .attr("y", 120)
+        .attr("x", 160)
+        .style("text-anchor", "start")
+        .style("font-family", "Monospace")
+        .text("//UA #175 WTC South");     //UA #175 WTC South
+var text5 = legendGroup.append("text")
+        .attr("y", 135)
+        .attr("x", 160)
+        .style("text-anchor", "start")
+        .style("font-family", "Monospace")
+        .text("Other Associates");       //Other Associates
